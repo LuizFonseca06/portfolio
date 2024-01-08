09eb4d63bbc4c559f5ce244becce8d1ebb3b5037
luizfonseca <luiz.fonseca@gerdau.com>
2024-01-07 21:38:43 -0500

    'summary' as list_display in admin.py

--- src/jobs/admin.py
+++ src/jobs/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Job
-# Register your models here.
 
-admin.site.register(Job)
+# Define a custom admin class
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('summary',)  # Add other fields if needed
+
+# Register the Job model with the custom admin class
+admin.site.register(Job, JobAdmin)
